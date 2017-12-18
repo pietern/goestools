056d0c8b0ade5db4fa7ca5a2cf0343d7c0709fd7
Pieter Noordhuis <pcnoordhuis@gmail.com>
2017-12-17 22:27:55 -0800

    Read packet data in goesdec
    Input files must be generated by the packetdump tool. If you hook up
    packetdump directly to the demodulation process then you no longer
    have to store demodulated data, and instead only deal with streams
    of packets. Consider this also prep for receiving a stream of packets
    over the network.

--- src/goesdec/main.cc
+++ src/goesdec/main.cc
@@ -1,12 +1,12 @@
 #include <assert.h>
-#include <malloc.h>
 #include <string.h>
 
 #include <array>
+#include <fstream>
 #include <iostream>
 #include <map>
+#include <vector>
 
-#include "packetizer.h"
 #include "file_handler.h"
 #include "vcdu.h"
 #include "virtual_channel.h"
@@ -18,13 +18,30 @@ int main(int argc, char** argv) {
     files.push_back(argv[i]);
   }
 
-  FileHandler handler("./out");
-  Packetizer p(std::make_shared<MultiFileReader>(files));
+  std::ifstream ifs;
   std::array<uint8_t, 892> buf;
-
   std::map<int, VirtualChannel> vcs;
-  for (auto j = 0; ; j++) {
-    p.nextPacket(buf, nullptr);
+  FileHandler handler("./out");
+  for (;;) {
+    // Make sure the ifstream is OK
+    if (!ifs.good() || ifs.eof()) {
+      if (files.size() == 0) {
+        break;
+      }
+      // Open next file
+      std::cerr << "Reading from: " << files.front() << std::endl;
+      ifs.close();
+      ifs.open(files.front());
+      assert(ifs.good());
+      files.erase(files.begin());
+    }
+
+    ifs.read((char*) buf.data(), buf.size());
+    if (ifs.eof()) {
+      continue;
+    }
+
+    // Got packet!
     VCDU vcdu(buf);
 
     // Ignore fill packets
