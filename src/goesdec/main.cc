a859e529ada05562cb8050d0458e83d7d6a311c4
Pieter Noordhuis <pcnoordhuis@gmail.com>
2018-01-16 23:42:06 -0800

    Let goesdec filter files by type

--- src/goesdec/main.cc
+++ src/goesdec/main.cc
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <getopt.h>
 #include <string.h>
 
 #include <array>
@@ -12,9 +13,52 @@
 #include "virtual_channel.h"
 
 int main(int argc, char** argv) {
+  bool images = false;
+  bool messages = false;
+  bool text = false;
+  bool dcs = false;
+
+  for (;;) {
+    static struct option longOpts[] = {
+      {"images", no_argument, 0, 0x1001},
+      {"messages", no_argument, 0, 0x1002},
+      {"text", no_argument, 0, 0x1003},
+      {"dcs", no_argument, 0, 0x1004},
+    };
+
+    int index;
+    auto c = getopt_long(argc, argv, "", longOpts, &index);
+    if (c == -1) {
+      break;
+    }
+
+    switch (c) {
+    case 0x1001:
+      images = true;
+      break;
+    case 0x1002:
+      messages = true;
+      break;
+    case 0x1003:
+      text = true;
+      break;
+    case 0x1004:
+      dcs = true;
+      break;
+    default:
+      std::cerr << "Invalid option" << std::endl;
+      exit(1);
+    }
+  }
+
+  if (!images && !dcs && !text) {
+    std::cerr << "Specify one of --images, --dcs, --text" << std::endl;
+    exit(1);
+  }
+
   // Turn argv into list of files
   std::vector<std::string> files;
-  for (int i = 1; i < argc; i++) {
+  for (int i = optind; i < argc; i++) {
     files.push_back(argv[i]);
   }
 
@@ -59,6 +103,20 @@ int main(int argc, char** argv) {
     auto it = vcs.find(vcid);
     auto spdus = it->second.process(vcdu);
     for (auto& spdu : spdus) {
+      auto type = spdu->getHeader<LRIT::PrimaryHeader>().fileType;
+      if (type == 0 && !images) {
+        continue;
+      }
+      if (type == 1 && !messages) {
+        continue;
+      }
+      if (type == 2 && !text) {
+        continue;
+      }
+      if (type == 130 && !dcs) {
+        continue;
+      }
+
       handler.handle(std::move(spdu));
     }
   }
