25cdd72f0f3b9cc7f906ec079a62f88c380cb014
Pieter Noordhuis <pcnoordhuis@gmail.com>
2018-01-18 20:51:51 -0800

    Move demodulator to its own file

---
+++ src/goesrecv/demodulator.cc
@@ -0,0 +1,108 @@
+#include "demodulator.h"
+
+Demodulator::Demodulator(Demodulator::Type t) {
+  switch (t) {
+  case LRIT:
+    freq_ = 1691000000;
+    symbolRate_ = 293883;
+    break;
+  case HRIT:
+    freq_ = 1694100000;
+    symbolRate_ = 927000;
+    break;
+  default:
+    assert(false);
+  }
+
+  // Sample rate depends on source
+  sampleRate_ = 0;
+
+  // Decimation depends on sample rate
+  decimationFactor_ = 1;
+
+  // Initialize queues
+  // It is possible to attach publishers before starting the demodulator
+  sourceQueue_ = std::make_shared<Queue<Samples> >(4);
+  agcQueue_ = std::make_shared<Queue<Samples> >(2);
+  rrcQueue_ = std::make_shared<Queue<Samples> >(2);
+  costasQueue_ = std::make_shared<Queue<Samples> >(2);
+  clockRecoveryQueue_ = std::make_shared<Queue<Samples> >(2);
+  softBitsQueue_ = std::make_shared<Queue<std::vector<int8_t> > >(2);
+
+  // Initialize publishers
+  sourcePublisher_ = Publisher::create("tcp://0.0.0.0:5003");
+  sourceQueue_->attach(std::move(sourcePublisher_));
+  clockRecoveryPublisher_ = Publisher::create("tcp://0.0.0.0:5002");
+  clockRecoveryQueue_->attach(std::move(clockRecoveryPublisher_));
+  softBitsPublisher_ = Publisher::create("tcp://0.0.0.0:5001");
+  softBitsQueue_->attach(std::move(softBitsPublisher_));
+}
+
+bool Demodulator::configureSource(const std::string& source) {
+  if (source == "airspy") {
+    sampleRate_ = 3000000;
+    airspy_ = Airspy::open();
+    airspy_->setCenterFrequency(freq_);
+    airspy_->setSampleRate(sampleRate_);
+    airspy_->setGain(18);
+    return true;
+  }
+
+  if (source == "rtlsdr") {
+    sampleRate_ = 2400000;
+    rtlsdr_ = RTLSDR::open();
+    rtlsdr_->setCenterFrequency(freq_);
+    rtlsdr_->setSampleRate(sampleRate_);
+    rtlsdr_->setTunerGain(30);
+    return true;
+  }
+
+  return false;
+}
+
+void Demodulator::start() {
+  auto sr1 = sampleRate_;
+  auto sr2 = sampleRate_ / decimationFactor_;
+  auto dc = decimationFactor_;
+
+  agc_ = std::make_unique<AGC>();
+  rrc_ = std::make_unique<RRC>(dc, sr1, symbolRate_);
+  costas_ = std::make_unique<Costas>();
+  clockRecovery_ = std::make_unique<ClockRecovery>(sr2, symbolRate_);
+  quantization_ = std::make_unique<Quantize>();
+
+  thread_ = std::thread([&] {
+      while (!sourceQueue_->closed()) {
+        agc_->work(sourceQueue_, agcQueue_);
+        rrc_->work(agcQueue_, rrcQueue_);
+        costas_->work(rrcQueue_, costasQueue_);
+        clockRecovery_->work(costasQueue_, clockRecoveryQueue_);
+        quantization_->work(clockRecoveryQueue_, softBitsQueue_);
+      }
+
+      // Close queues to signal downstream consumers of termination
+      agcQueue_->close();
+      rrcQueue_->close();
+      costasQueue_->close();
+      clockRecoveryQueue_->close();
+      softBitsQueue_->close();
+    });
+
+  if (airspy_) {
+    airspy_->start(sourceQueue_);
+  }
+  if (rtlsdr_) {
+    rtlsdr_->start(sourceQueue_);
+  }
+}
+
+void Demodulator::stop() {
+  if (airspy_) {
+    airspy_->stop();
+  }
+  if (rtlsdr_) {
+    rtlsdr_->stop();
+  }
+
+  thread_.join();
+}
---
+++ src/goesrecv/demodulator.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "agc.h"
+#include "airspy_source.h"
+#include "clock_recovery.h"
+#include "costas.h"
+#include "publisher.h"
+#include "quantize.h"
+#include "rrc.h"
+#include "rtlsdr_source.h"
+#include "types.h"
+
+class Demodulator {
+public:
+  enum Type {
+    LRIT = 1,
+    HRIT = 2,
+  };
+
+  explicit Demodulator(Type t);
+
+  std::shared_ptr<Queue<std::vector<int8_t> > > getSoftBitsQueue() {
+    return softBitsQueue_;
+  }
+
+  bool configureSource(const std::string& source);
+
+  void start();
+  void stop();
+
+protected:
+  uint32_t freq_;
+  uint32_t symbolRate_;
+  uint32_t sampleRate_;
+  uint32_t decimationFactor_;
+
+  std::thread thread_;
+
+  // Sources (only one is used)
+  std::unique_ptr<Airspy> airspy_;
+  std::unique_ptr<RTLSDR> rtlsdr_;
+
+  // DSP blocks
+  std::unique_ptr<AGC> agc_;
+  std::unique_ptr<RRC> rrc_;
+  std::unique_ptr<Costas> costas_;
+  std::unique_ptr<ClockRecovery> clockRecovery_;
+  std::unique_ptr<Quantize> quantization_;
+
+  // Queues
+  std::shared_ptr<Queue<Samples> > sourceQueue_;
+  std::shared_ptr<Queue<Samples> > agcQueue_;
+  std::shared_ptr<Queue<Samples> > rrcQueue_;
+  std::shared_ptr<Queue<Samples> > costasQueue_;
+  std::shared_ptr<Queue<Samples> > clockRecoveryQueue_;
+  std::shared_ptr<Queue<std::vector<int8_t> > > softBitsQueue_;
+
+  // Publishers (so queues can be monitored externally)
+  std::unique_ptr<Publisher> sourcePublisher_;
+  std::unique_ptr<Publisher> clockRecoveryPublisher_;
+  std::unique_ptr<Publisher> softBitsPublisher_;
+};
