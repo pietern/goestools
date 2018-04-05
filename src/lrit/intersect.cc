e3023f67552acf1219b52edc46d62dc8d09e313b
Pieter Noordhuis <pcnoordhuis@gmail.com>
2018-04-04 21:45:03 -0700

    Add tool to compute area intersection
    This computes the largest intersection between areas covered by
    different products. It spits out a crop value that can be used by
    goesproc. The resulting images from different products can then be
    used in sequence to get more frequent imagery of a smaller area.
    
    This is largely copied from the previous goesproc version where the
    largest intersecting area was computed on the fly. It cannot be done
    on the fly in streaming mode, so with the current goesproc version you
    need to precompute the cropping coordinates.
    
    This is only relevant for the GOES-N series products because GOES-R
    doesn't have different fixed regions (the only fixed region is the
    full disk).

---
+++ src/lrit/intersect.cc
@@ -0,0 +1,119 @@
+#include <algorithm>
+#include <map>
+#include <vector>
+
+#include "file.h"
+
+struct Area {
+  int minColumn = 0;
+  int maxColumn = 0;
+  int minLine = 0;
+  int maxLine = 0;
+
+  int width() const {
+    return maxColumn - minColumn;
+  }
+
+  int height() const {
+    return maxLine - minLine;
+  }
+
+  Area getIntersection(const Area& other) const {
+    Area result;
+    result.minColumn = std::max(minColumn, other.minColumn);
+    result.maxColumn = std::min(maxColumn, other.maxColumn);
+    result.minLine = std::max(minLine, other.minLine);
+    result.maxLine = std::min(maxLine, other.maxLine);
+    return result;
+  }
+
+  Area getUnion(const Area& other) const {
+    Area result;
+    result.minColumn = std::min(minColumn, other.minColumn);
+    result.maxColumn = std::max(maxColumn, other.maxColumn);
+    result.minLine = std::min(minLine, other.minLine);
+    result.maxLine = std::max(maxLine, other.maxLine);
+    return result;
+  }
+};
+
+// Compute intersection of area covered by specified images.
+int main(int argc, char** argv) {
+  std::map<int, std::vector<lrit::File>> all;
+
+  // Group files by their image identifier
+  for (int i = 1; i < argc; i++) {
+    auto file = lrit::File(argv[i]);
+    if (!file.hasHeader<lrit::ImageNavigationHeader>()) {
+      continue;
+    }
+
+    auto sih = file.getHeader<lrit::SegmentIdentificationHeader>();
+    auto& files = all[sih.imageIdentifier];
+    files.push_back(std::move(file));
+  }
+
+  // Compute area intersection between all images
+  Area out;
+  for (auto& it : all) {
+    auto& files = it.second;
+
+    // Sort by segment identifier
+    std::sort(
+      files.begin(),
+      files.end(),
+      [](const auto& a, const auto& b) -> bool {
+        auto sa = a.template getHeader<lrit::SegmentIdentificationHeader>();
+        auto sb = b.template getHeader<lrit::SegmentIdentificationHeader>();
+        return sa.segmentNumber < sb.segmentNumber;
+      });
+
+    // Ignore incomplete images
+    auto si = files[0].getHeader<lrit::SegmentIdentificationHeader>();
+    if (files.size() != si.maxSegment) {
+      continue;
+    }
+
+    // Compute area covered by this segmented image.
+    // Largely copied from the original version of goesproc,
+    // which was removed in eaa56cb2.
+    Area area;
+    for (auto& file : files) {
+      auto is = file.getHeader<lrit::ImageStructureHeader>();
+      auto in = file.getHeader<lrit::ImageNavigationHeader>();
+      auto nl = file.getHeader<lrit::NOAALRITHeader>();
+
+      // Update relative area represented by this image
+      Area tmp;
+      tmp.minColumn = -in.columnOffset;
+      tmp.maxColumn = -in.columnOffset + is.columns;
+      tmp.minLine = -in.lineOffset;
+      tmp.maxLine = -in.lineOffset + is.lines;
+
+      // For Himawari-8, the line offset is an int32_t and can be negative
+      if (nl.productID == 43) {
+        tmp.minLine = -(int32_t)in.lineOffset;
+        tmp.maxLine = -(int32_t)in.lineOffset + is.lines;
+      }
+
+      if (area.height() == 0) {
+        area = tmp;
+      } else {
+        area = area.getUnion(tmp);
+      }
+    }
+
+    if (out.height() == 0) {
+      out = area;
+    } else {
+      out = out.getIntersection(area);
+    }
+  }
+
+  std::cout << "crop = [ ";
+  std::cout << out.minColumn << ", ";
+  std::cout << out.maxColumn << ", ";
+  std::cout << out.minLine << ", ";
+  std::cout << out.maxLine << " ]";
+  std::cout << std::endl;
+}
