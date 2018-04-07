c64e5508614ea6e06300f27e0a7d2ec2b8cda450
Pieter Noordhuis <pcnoordhuis@gmail.com>
2018-04-06 23:12:10 -0700

    Make goesproc log to stdout

--- src/goesproc/file_writer.cc
+++ src/goesproc/file_writer.cc
@@ -15,21 +15,21 @@ FileWriter::~FileWriter() {
 
 void FileWriter::write(const std::string& path, const cv::Mat& mat) {
   if (!tryWrite(path)) {
-    std::cerr << "Skipping (file exists): " << path << std::endl;
+    std::cout << "Skipping (file exists): " << path << std::endl;
     return;
   }
 
-  std::cerr << "Writing: " << path << std::endl;
+  std::cout << "Writing: " << path << std::endl;
   cv::imwrite(path, mat);
 }
 
 void FileWriter::write(const std::string& path, const std::vector<char>& data) {
   if (!tryWrite(path)) {
-    std::cerr << "Skipping (file exists): " << path << std::endl;
+    std::cout << "Skipping (file exists): " << path << std::endl;
     return;
   }
 
-  std::cerr << "Writing: " << path << std::endl;
+  std::cout << "Writing: " << path << std::endl;
   std::ofstream of(path);
   of.write(data.data(), data.size());
 }
