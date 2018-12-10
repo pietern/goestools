48b59119e923065a4d7dd41b820698acfc65e311
Pieter Noordhuis <pcnoordhuis@gmail.com>
2018-12-09 20:50:56 -0800

    Add custom assert macro

---
+++ src/util/error.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <exception>
+
+#include <util/string.h>
+
+#define ERROR(...) throw std::runtime_error(::util::str(__VA_ARGS__))
+
+#define ASSERT(cond)                                                      \
+  do {                                                                    \
+    if (!(cond)) {                                                        \
+      ERROR("Assertion `" #cond "` failed at ", __FILE__, ":", __LINE__); \
+    }                                                                     \
+  } while (0)
+
+#define ASSERTM(cond, ...)                    \
+  do {                                        \
+    if (!(cond)) {                            \
+      ERROR(                                  \
+          "Assertion `" #cond "` failed at ", \
+          __FILE__,                           \
+          ":",                                \
+          __LINE__,                           \
+          ", ",                               \
+          ::util::str(__VA_ARGS__));          \
+    }                                         \
+  } while (0)
--- src/util/string.h
+++ src/util/string.h
@@ -1,9 +1,47 @@
 #pragma once
 
+#include <sstream>
 #include <string>
 #include <vector>
 
 namespace util {
+
+namespace detail {
+
+inline std::ostream& _str(std::ostream& ss) {
+  return ss;
+}
+
+template <typename T>
+inline std::ostream& _str(std::ostream& ss, const T& t) {
+  ss << t;
+  return ss;
+}
+
+template <typename T, typename... Args>
+inline std::ostream& _str(std::ostream& ss, const T& t, const Args&... args) {
+  return _str(_str(ss, t), args...);
+}
+
+} // namespace detail
+
+// Convert a list of string-like arguments into a single string.
+template <typename... Args>
+inline std::string str(const Args&... args) {
+  std::ostringstream ss;
+  detail::_str(ss, args...);
+  return ss.str();
+}
+
+// Specializations for already-a-string types.
+template <>
+inline std::string str(const std::string& str) {
+  return str;
+}
+
+inline std::string str(const char* c_str) {
+  return c_str;
+}
 
 std::vector<std::string> split(std::string in, char delim);
 
