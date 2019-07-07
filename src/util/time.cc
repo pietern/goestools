170b870d180c4d02a38d0659b5ec4d000a63bc52
whiten01se <github@laleli.lu>
2019-07-07 14:40:31 -0400

    Issue #55: Initializing tm struct before use with mktime(). (#56)
    * Issue #55: Initializing tm struct before use with mktime().
    
    * Fixed comment style

--- src/util/time.cc
+++ src/util/time.cc
@@ -1,7 +1,7 @@
 #include "time.h"
 
 #include <array>
-
+#include <cstring>
 #include <util/error.h>
 
 namespace util {
@@ -22,6 +22,10 @@ std::string stringTime() {
 bool parseTime(const std::string& in, struct timespec* ts) {
   const char* buf = in.c_str();
   struct tm tm;
+
+  // Initialize tm struct to 0s before using it, otherwise mktime will behave unpredictably - Kons
+  memset(&tm, 0x0, sizeof(tm));
+
   long int tv_nsec = 0;
 
   // For example: 2017-12-21T17:46:32.2Z
@@ -42,6 +46,10 @@ bool parseTime(const std::string& in, struct timespec* ts) {
   }
 
   auto t = mktime(&tm);
+
+  // The resulting t should always be greater 0, otherwise there was a problem with date conversion
+  ASSERT(t > 0);
+
   ts->tv_sec = t;
   ts->tv_nsec = tv_nsec;
   return true;
