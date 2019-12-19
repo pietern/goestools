d03ba3c617590ed688042ff0fc57a47d91033965
Jim Herbert <jim.herbert@me.com>
2019-12-19 15:56:30 -0500

    clang formatting changes to better match the original project

--- src/goesrecv/uhd_source.cc
+++ src/goesrecv/uhd_source.cc
@@ -12,11 +12,11 @@
 #include <uhd/utils/thread_priority.hpp>
 #include <util/error.h>
 
-std::unique_ptr<UHD> UHD::open( std::string type ) {
+std::unique_ptr<UHD> UHD::open(std::string type) {
     std::string args = "";
 
     // Filter to a device type if one was specified
-    if ( not type.empty() ) {
+    if (not type.empty()) {
         args = "type=" + type;
     }
 
@@ -26,25 +26,25 @@ std::unique_ptr<UHD> UHD::open( std::string type ) {
     // Create a USRP device
     uhd::usrp::multi_usrp::sptr dev = nullptr;
 
-    UHD_SAFE_CALL( dev = uhd::usrp::multi_usrp::make( args ); )
+    UHD_SAFE_CALL(dev = uhd::usrp::multi_usrp::make(args);)
 
     // Abort if we didn't open a device
-    if ( dev == nullptr ) {
+    if (dev == nullptr) {
         std::cout << "Unable to open UHD device. " << std::endl;
 
-        exit( 1 );
+        exit(1);
     }
 
     std::cout << std::endl
               << "Using Device " << dev->get_pp_string() << std::endl;
 
     // Select the internal clock source
-    dev->set_clock_source( "internal" );
+    dev->set_clock_source("internal");
 
-    return std::make_unique<UHD>( dev );
+    return std::make_unique<UHD>(dev);
 }
 
-UHD::UHD( uhd::usrp::multi_usrp::sptr dev ) : dev_( dev ) {
+UHD::UHD(uhd::usrp::multi_usrp::sptr dev) : dev_(dev) {
     // Perform necessary constructor operations here
 }
 
@@ -52,42 +52,42 @@ UHD::~UHD() {
     // Perform necessary destructor operations here
 }
 
-void UHD::setFrequency( uint32_t freq ) {
-    ASSERT( dev_ != nullptr );
+void UHD::setFrequency(uint32_t freq) {
+    ASSERT(dev_ != nullptr);
 
-    uhd::tune_request_t tune_request( freq );
+    uhd::tune_request_t tune_request(freq);
 
-    dev_->set_rx_freq( tune_request );
+    dev_->set_rx_freq(tune_request);
 }
 
-void UHD::setSampleRate( uint32_t rate ) {
-    ASSERT( dev_ != nullptr );
+void UHD::setSampleRate(uint32_t rate) {
+    ASSERT(dev_ != nullptr);
 
-    dev_->set_rx_rate( rate );
+    dev_->set_rx_rate(rate);
 
     // Set the bandwidth to match the sample rate
-    dev_->set_rx_bandwidth( rate );
+    dev_->set_rx_bandwidth(rate);
 }
 
 uint32_t UHD::getSampleRate() const {
-    ASSERT( dev_ != nullptr );
+    ASSERT(dev_ != nullptr);
 
     return dev_->get_rx_rate();
 }
 
-void UHD::setGain( int gain ) {
-    ASSERT( dev_ != nullptr );
+void UHD::setGain(int gain) {
+    ASSERT(dev_ != nullptr);
 
-    dev_->set_rx_gain( gain );
+    dev_->set_rx_gain(gain);
 }
 
-void UHD::start( const std::shared_ptr<Queue<Samples>> &queue ) {
-    ASSERT( dev_ != nullptr );
+void UHD::start(const std::shared_ptr<Queue<Samples>> &queue) {
+    ASSERT(dev_ != nullptr);
 
     // This is the application's sample queue
     queue_ = queue;
 
-    thread_ = std::thread( [&] {
+    thread_ = std::thread([&] {
         // The streamming loop continues to run as long as this is true
         running = true;
 
@@ -95,70 +95,70 @@ void UHD::start( const std::shared_ptr<Queue<Samples>> &queue ) {
         size_t num_acc_samps = 0;
 
         // Set the antenana for receiving our signal
-        dev_->set_rx_antenna( "TX/RX" );
+        dev_->set_rx_antenna("TX/RX");
 
         // Check sensors for a local oscillator
-        std::vector<std::string> sensor_names = dev_->get_rx_sensor_names( 0 );
+        std::vector<std::string> sensor_names = dev_->get_rx_sensor_names(0);
 
         // If a local oscillator exists, make sure it's locked
-        if ( std::find( sensor_names.begin(), sensor_names.end(),
-                        "lo_locked" ) != sensor_names.end() ) {
+        if (std::find(sensor_names.begin(), sensor_names.end(), "lo_locked") !=
+            sensor_names.end()) {
             std::cout << std::endl
                       << "-- Found a local oscillator" << std::endl;
 
             int lock_attempts = 0;
 
             do {
-                if ( lock_attempts++ > 3 ) // Abort after three attempts
+                if (lock_attempts++ > 3) // Abort after three attempts
                 {
                     std::cout << "Unable to lock local oscillator" << std::endl;
 
-                    exit( 1 );
+                    exit(1);
                 }
 
                 // Allow the front end time to settle
-                std::this_thread::sleep_for( std::chrono::seconds( 1 ) );
-            } while ( not dev_->get_rx_sensor( "lo_locked" ).to_bool() );
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+            } while (not dev_->get_rx_sensor("lo_locked").to_bool());
 
             std::cout << "-- Local oscillator locked" << std::endl << std::endl;
         }
 
         // Specify that we want to receive samples as a pair of 16 bit complex
         // foats
-        uhd::stream_args_t stream_args( "fc32" );
+        uhd::stream_args_t stream_args("fc32");
 
         // Create a receive stream
-        uhd::rx_streamer::sptr rx_stream = dev_->get_rx_stream( stream_args );
+        uhd::rx_streamer::sptr rx_stream = dev_->get_rx_stream(stream_args);
 
         // Create a stream command that will start immediately and run
         // continuously
         uhd::stream_cmd_t stream_cmd_start(
-            uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS );
+            uhd::stream_cmd_t::STREAM_MODE_START_CONTINUOUS);
 
         stream_cmd_start.stream_now = true;
 
         // Start streaming
-        rx_stream->issue_stream_cmd( stream_cmd_start );
+        rx_stream->issue_stream_cmd(stream_cmd_start);
 
         // Holds stream metadata, such as error codes
         uhd::rx_metadata_t md;
 
         // Create an internal buffer to hold received samples
-        std::vector<std::complex<float>> buff( rx_stream->get_max_num_samps() *
-                                               2 );
+        std::vector<std::complex<float>> buff(rx_stream->get_max_num_samps() *
+                                              2);
 
-        while ( running ) {
+        while (running) {
             // Read samples from the device and into our buffer
             size_t num_rx_samps =
-                rx_stream->recv( &buff.front(), buff.size(), md );
+                rx_stream->recv(&buff.front(), buff.size(), md);
 
             // Handle any error codes we receive
-            switch ( md.error_code ) {
+            switch (md.error_code) {
             case uhd::rx_metadata_t::ERROR_CODE_NONE:
                 break;
 
             case uhd::rx_metadata_t::ERROR_CODE_TIMEOUT:
-                if ( num_acc_samps == 0 )
+                if (num_acc_samps == 0)
                     continue;
 
                 std::cerr << "Timeout before all samples received, possible "
@@ -168,7 +168,7 @@ void UHD::start( const std::shared_ptr<Queue<Samples>> &queue ) {
                 goto done_loop;
 
             case uhd::rx_metadata_t::ERROR_CODE_OVERFLOW:
-                if ( md.out_of_sequence == true )
+                if (md.out_of_sequence == true)
                     std::cerr << "Samples out of sequence" << std::endl;
                 else
                     std::cerr << "Sample buffer overflow.  If this persists, "
@@ -187,12 +187,12 @@ void UHD::start( const std::shared_ptr<Queue<Samples>> &queue ) {
 
             // The receiving queue is expecting a number of samples evenly
             // divisible by four
-            if ( num_rx_samps % 4 != 0 )
+            if (num_rx_samps % 4 != 0)
                 continue;
 
             // Copies samples from our internal buffer into the application's
             // queue
-            transfer_buffer( buff, num_rx_samps );
+            transfer_buffer(buff, num_rx_samps);
 
             // Increment the accumulated sample count
             num_acc_samps += num_rx_samps;
@@ -201,27 +201,27 @@ void UHD::start( const std::shared_ptr<Queue<Samples>> &queue ) {
 
         // setup streaming
         uhd::stream_cmd_t stream_cmd_stop(
-            uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS );
+            uhd::stream_cmd_t::STREAM_MODE_STOP_CONTINUOUS);
 
         // stream_cmd.stream_now = true;
 
-        rx_stream->issue_stream_cmd( stream_cmd_stop );
+        rx_stream->issue_stream_cmd(stream_cmd_stop);
 
         // finished
         std::cout << std::endl << "Streaming ended" << std::endl << std::endl;
 
         return EXIT_SUCCESS;
-    } );
+    });
 
 #ifdef __APPLE__
-    pthread_setname_np( "uhd" );
+    pthread_setname_np("uhd");
 #else
-    pthread_setname_np( thread_.native_handle(), "uhd" );
+    pthread_setname_np(thread_.native_handle(), "uhd");
 #endif
 }
 
 void UHD::stop() {
-    ASSERT( dev_ != nullptr );
+    ASSERT(dev_ != nullptr);
 
     // Causes the sample streaming loop to exit
     running = false;
@@ -237,27 +237,27 @@ void UHD::stop() {
 }
 
 // Copies samples from our internal buffer into the application's queue
-void UHD::transfer_buffer( std::vector<std::complex<float>> transfer,
-                           size_t num_rx_samps ) {
+void UHD::transfer_buffer(std::vector<std::complex<float>> transfer,
+                          size_t num_rx_samps) {
     // Expect multiple of 2
-    ASSERT( ( num_rx_samps & 0x2 ) == 0 );
+    ASSERT((num_rx_samps & 0x2) == 0);
 
     // Grab buffer from queue
     auto out = queue_->popForWrite();
 
-    out->resize( num_rx_samps );
+    out->resize(num_rx_samps);
 
     std::complex<float> *p = out->data();
 
-    for ( int i = 0; i < int( num_rx_samps ); i++ ) {
+    for (int i = 0; i < int(num_rx_samps); i++) {
         p[i] = transfer[i];
     }
 
     // Publish output if applicable
-    if ( samplePublisher_ ) {
-        samplePublisher_->publish( *out );
+    if (samplePublisher_) {
+        samplePublisher_->publish(*out);
     }
 
     // Return buffer to queue
-    queue_->pushWrite( std::move( out ) );
+    queue_->pushWrite(std::move(out));
 }
--- src/goesrecv/uhd_source.h
+++ src/goesrecv/uhd_source.h
@@ -1,62 +1,60 @@
 #pragma once
 
-#include <stdint.h>
+#include "source.h"
 #include <memory>
+#include <stdint.h>
 #include <thread>
-#include <vector>
 #include <uhd.h>
 #include <uhd/exception.hpp>
 #include <uhd/usrp/multi_usrp.hpp>
 #include <uhd/utils/safe_main.hpp>
-#include "source.h"
+#include <vector>
 
-class UHD : public Source
-{
-    public:
-        static std::unique_ptr<UHD> open( std::string type );
+class UHD : public Source {
+  public:
+    static std::unique_ptr<UHD> open(std::string type);
 
-        explicit UHD( uhd::usrp::multi_usrp::sptr dev );
+    explicit UHD(uhd::usrp::multi_usrp::sptr dev);
 
-        ~UHD( );
+    ~UHD();
 
-        void setFrequency( uint32_t freq );
+    void setFrequency(uint32_t freq);
 
-        // Also sets the bandwidth to match
-        void setSampleRate( uint32_t rate );
+    // Also sets the bandwidth to match
+    void setSampleRate(uint32_t rate);
 
-        // Reads the current sample rate setting from the device
-        virtual uint32_t getSampleRate( ) const override;
+    // Reads the current sample rate setting from the device
+    virtual uint32_t getSampleRate() const override;
 
-        void setGain( int gain );
+    void setGain(int gain);
 
-        void setSamplePublisher( std::unique_ptr<SamplePublisher> samplePublisher )
-        {
-            samplePublisher_ = std::move( samplePublisher );
-        }
+    void setSamplePublisher(std::unique_ptr<SamplePublisher> samplePublisher) {
+        samplePublisher_ = std::move(samplePublisher);
+    }
 
-        // Starts the sample streaming loop
-        virtual void start( const std::shared_ptr<Queue<Samples>> &queue ) override;
+    // Starts the sample streaming loop
+    virtual void start(const std::shared_ptr<Queue<Samples>> &queue) override;
 
-        // Stops the sample streaming loop
-        virtual void stop( ) override;
+    // Stops the sample streaming loop
+    virtual void stop() override;
 
-    protected:
+  protected:
+    // When set to false the sample streaming loop exits
+    bool running;
 
-        // When set to false the sample streaming loop exits
-        bool running;
+    // A reference to the device
+    uhd::usrp::multi_usrp::sptr dev_;
 
-        // A reference to the device
-        uhd::usrp::multi_usrp::sptr dev_;
+    // Background RX thread
+    std::thread thread_;
 
-        // Background RX thread
-        std::thread thread_;
+    // Copies samples from our internal buffer into the application's queue
+    virtual void transfer_buffer(std::vector<std::complex<float>> transfer,
+                                 size_t num_rx_samps);
 
-        // Copies samples from our internal buffer into the application's queue
-        virtual void transfer_buffer( std::vector<std::complex<float>> transfer, size_t num_rx_samps );
+    // Set on start; cleared on stop
+    std::shared_ptr<Queue<Samples>> queue_;
 
-        // Set on start; cleared on stop
-        std::shared_ptr<Queue<Samples> > queue_;
-
-        // Optional publisher for samples
-        std::unique_ptr<SamplePublisher> samplePublisher_;
+    // Optional publisher for samples
+    std::unique_ptr<SamplePublisher> samplePublisher_;
 };
