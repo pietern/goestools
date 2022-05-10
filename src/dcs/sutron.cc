f237518648e424fc0502a0866afc0720b65d6c1b
Tom Cutter <tcutter@noping.net>
2022-05-10 13:18:25 -0700

    consider MSB to be negative, and follow through on final value being negative

--- src/dcs/sutron.cc
+++ src/dcs/sutron.cc
@@ -127,8 +127,6 @@ std::string	sutron(std::string inbuf)
 				    else	
 				    {
 					r= (inbuf[i+2] - 64)&0x3F; // right of 18 bit percision
-				        if ((inbuf[i+2] & 0x20) !=0 )
-								sign = -1; // negative 
 				    }
 					
 				    if (i+1 > len) 
@@ -137,6 +135,8 @@ std::string	sutron(std::string inbuf)
 				    	m= ((inbuf[i+1] - 64)&0x3F) <<6; // middle of 18 bit percision
 
 			  	    l= ((inbuf[i] - 64)&0x3F) <<12; // left of 18 bit percision
+				    if ( (l & 0x20) != 0)
+							sign = -1; // negative 
 				    ival= l + m + r;
 				    if (sign == -1)
 					ival=ival*sign; // negative value
@@ -216,8 +216,6 @@ std::string	sutron(std::string inbuf)
                                     	else
 					{
                                         	r= (sensor[i+2] - 64)&0x3F; // right of 18 bit percision
-				   		if ((inbuf[i+2] & 0x20) !=0 )
-									sign = -1;
 					}
 
                                     	if (i+1 > len)
@@ -233,6 +231,8 @@ std::string	sutron(std::string inbuf)
 					{
                                     		l= ((sensor[i] - 64)&0x3F) <<12; // left of 18 bit percision
 					}
+				   	if ((l & 0x20) !=0 )
+								sign = -1;
 
                                     	ival= l + m + r;
 					if ( sign == -1)
@@ -310,8 +310,6 @@ std::string	sutron(std::string inbuf)
 				    else	
 				    {
 					r= (inbuf[i+2] - 64)&0x3F; // right of 18 bit percision
-					if ((inbuf[i+2] & 0x20) !=0 )
-                                       		                 sign = -1;
 				    }
 					
 				    if (i+1 > len) 
@@ -320,6 +318,8 @@ std::string	sutron(std::string inbuf)
 				    	m= ((inbuf[i+1] - 64)&0x3F) <<6; // middle of 18 bit percision
 
 			  	    l= ((inbuf[i] - 64)&0x3F) <<12; // left of 18 bit percision
+				    if ((l & 0x20) !=0 )
+                                                         sign = -1;
 				    ival= l + m + r;
 				    if (sign == -1)
 				    	ival=ival*sign; // negative value
